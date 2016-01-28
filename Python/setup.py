ca5c36c9792161d118ff234bf4efe3510c565e92
Andres Jordan <andres.jordan@gmail.com>
2016-01-28 11:19:08 -0300

    removed M&A code

--- Python/setup.py
+++ Python/setup.py
@@ -4,7 +4,7 @@ def configuration(parent_package='',top_path=None):
     base = '../../'
     from numpy.distutils.misc_util import Configuration
     config = Configuration('',parent_package,top_path)
-    config.add_extension('ajplanet',sources=[base+'ajplanet/Python/ajplanet.pyf', base+'ajplanet/convlib.c',base+'ajplanet/true_anomaly.c',base+'ajplanet/rv.c',base+'ajplanet/orbit.c',base+'ajplanet/occultquad.f',base+'ajplanet/occultnl.f'],include_dirs=[base+'ajplanet/', '/usr/local/include'],library_dirs=['/usr/local/lib'], libraries=['gsl','gslcblas','m'])
+    config.add_extension('ajplanet',sources=[base+'ajplanet/Python/ajplanet.pyf', base+'ajplanet/convlib.c',base+'ajplanet/true_anomaly.c',base+'ajplanet/rv.c',base+'ajplanet/orbit.c'],include_dirs=[base+'ajplanet/', '/usr/local/include'],library_dirs=['/usr/local/lib'], libraries=['gsl','gslcblas','m'])
     return config
 
 if __name__ == "__main__":
